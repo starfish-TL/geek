2862e2ef382493a7678d97e64089dc4e781a50e4
tianlu <tianlu>
2021-07-04 18:50:47 +0800

    week_02

---
+++ JavaCourse/week_02/src/main/java/HttpClientUtil.java
@@ -0,0 +1,60 @@
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+import java.io.IOException;
+
+/**
+ * @author tianlu
+ * @date 2021/7/4
+ * <p>
+ * 写一段代码，使用HttpClient或OkHttp访问http://localhost:8801，代码提交到GitHub
+ */
+public class HttpClientUtil {
+    public static void main(String[] args) {
+        String url = "http://localhost:8801";
+        // 1.生成HttpClient对象并设置参数
+        HttpClient httpClient = new HttpClient();
+        // 设置Http连接超时为5秒
+        httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(3000);
+        // 2.生成GetMethod对象并设置参数
+        GetMethod getMethod = new GetMethod(url);
+        // 设置get请求超时为3秒
+        getMethod.getParams().setParameter(HttpMethodParams.SO_TIMEOUT, 3000);
+        // 设置请求重试处理，用的是默认的重试处理：请求三次
+        getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
+        String response = "";
+        // 3.执行HTTP GET请求
+        try {
+            int statusCode = httpClient.executeMethod(getMethod);
+            // 4.判断访问的状态码
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("请求出错：" + getMethod.getStatusLine());
+            }
+            // 5.处理HTTP响应内容
+            // HTTP响应头部信息，这里简单打印
+            Header[] headers = getMethod.getResponseHeaders();
+            for (Header h : headers) {
+                System.out.println(h.getName() + "---------------" + h.getValue());
+            }
+            // 读取HTTP响应内容，这里简单打印网页内容
+            // 读取为字节数组
+            byte[] responseBody = getMethod.getResponseBody();
+            response = new String(responseBody, "utf-8");
+            System.out.println("-----------response:" + response);
+            // 读取为InputStream，在网页内容数据量大时候推荐使用
+            // InputStream response = getMethod.getResponseBodyAsStream();
+        } catch (HttpException e) {
+            // 发生致命的异常，可能是协议不对或者返回的内容有问题
+            System.out.println("请检查输入的URL!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 发生网络异常
+            System.out.println("发生网络异常!");
+        } finally {
+            // 6.释放连接
+            getMethod.releaseConnection();
+        }
+    }
+
+}
